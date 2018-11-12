8560d6b31610128bfa8a7a47c19c7ce5f79a9ee5
Jake-Harris2 <44767481+Jake-Harris2@users.noreply.github.com>
2018-11-12 10:59:03 +0000

    Tried To Do Scrolling Background
    Still Needs Lots of fixing

--- Defender Game/Assets/Scripts/Background.cs
+++ Defender Game/Assets/Scripts/Background.cs
@@ -8,19 +8,24 @@ public class Background : MonoBehaviour
     [SerializeField]
     float backgroundScrollMultiplier = 60f;
 
-    Rigidbody2D playerRigidbody;
+    private Renderer backgroundRenderer;
+
+    Vector2 backgroundStartingPosition;
 
 	void Start ()
     {
-        playerRigidbody = GetComponent<Rigidbody2D>();
+        backgroundRenderer = GetComponent<Renderer>();
 	}
 	
 	void Update ()
     {
         backgroundScrollingSpeed = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMotor>().GetBackgroundScrollSpeed()*backgroundScrollMultiplier;
-        Debug.Log(backgroundScrollingSpeed);
-        Vector2 backgroundMovement = Vector2.right * -backgroundScrollingSpeed;
-        playerRigidbody.MovePosition(playerRigidbody.position + backgroundMovement * Time.deltaTime);
-        //Create empty gameobjects to use onbecamevisible() to check for the end of the texture
+        float backgroundMovement = backgroundScrollMultiplier * Time.time;
+        Vector2 backgroundOffset = new Vector2(backgroundMovement, 0);
+        //Debug.Log(backgroundScrollingSpeed);
+        backgroundRenderer.material.mainTextureOffset = backgroundOffset;
+
+        /*Vector2 backgroundMovement = Vector2.right * -backgroundScrollingSpeed;
+        transform.position = new Vector2(transform.position.x, transform.position.y) + backgroundMovement * Time.deltaTime;*/
     }
 }
